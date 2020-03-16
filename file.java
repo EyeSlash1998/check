8713306ad8f678ae3ebd997b8e1338db17f1b7b0
EyeSlash1998 <36436501+EyeSlash1998@users.noreply.github.com>
2020-03-16 14:15:31 +0530

    myMethod made static and called.

--- file.java
+++ file.java
@@ -1,10 +1,11 @@
 class file{
 
-	void myMethod(){
+	static void myMethod(){
 		sysout("New branch");
 	}
 
 	public static void main(String args[]){
 		system.out.println("Hello Git, Its DevOps");
+		MyClass.myMethod();
 	}
 }
